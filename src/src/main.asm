ffc2a955350dd3febf15631e21953272768a9374
Thoma55s <thomas.morgan.sager@gmail.com>
2018-04-03 20:44:01 -0400

    Added Shop

--- src/src/main.asm
+++ src/src/main.asm
@@ -21,22 +21,24 @@ include enemy.inc
 include combat.inc
 include levelUp.inc
 include inventory.inc
+include shop.inc
 
 WriteDec proto
 ReadDec proto
 ExitProcess proto, dwExitCode:dword
 
 mainMenuTitle    byte "Main Menu"        , 13, 10, 0
-quitTitle        byte "0 Quit"           , 13, 10, 0
+quitTitle        byte "909 Quit"           , 13, 10, 0
 characterSheet   byte "1 Character Sheet", 13, 10, 0
 combatTitle      byte "2 Combat"         , 13, 10, 0
 levelUpTitle     byte "3 Level Up"       , 13, 10, 0
 inventoryTitle   byte "4 Inventory"      , 13, 10, 0
 dequipTitle      byte "5 Dequip"         , 13, 10, 0
+shopTitle        byte "6 Shop"           , 13, 10, 0
 
 continueCommand byte "Press Enter to Continue",13, 10, 0
 
-
+exitTitle    byte "909 Exit: ", 13, 10, 0
 
 blank byte " ", 13, 10, 0
 
@@ -56,8 +58,8 @@ while_main:						;//while( ecx != 0 )
 	
 	mov edx, offset mainMenuTitle ;//Main Menu Display Start
 	call WriteString
-		; mov edx, offset quitTitle
-		; call WriteString
+		mov edx, offset quitTitle
+		call WriteString
 		mov edx, offset characterSheet
 		call WriteString
 		mov edx, offset combatTitle
@@ -68,13 +70,15 @@ while_main:						;//while( ecx != 0 )
 		call WriteString
 		mov edx, offset dequipTitle
 		call WriteString
+		mov edx, offset shopTitle
+		call WriteString
 
 		_get_input
 
 		call Clrscr
 
-		; cmp ecx, 0;//switch
-	; je endwhile_main
+	cmp ecx, 909;//switch
+	je endwhile_main
 	cmp ecx, 1
 	je character_sheet
 	cmp ecx, 2
@@ -85,6 +89,8 @@ while_main:						;//while( ecx != 0 )
 	je inventory
 	cmp ecx, 5
 	je dequip
+	cmp ecx, 6
+	je shop
 	jmp end_switch
 
 	character_sheet:
@@ -102,11 +108,14 @@ while_main:						;//while( ecx != 0 )
 	dequip:
 		mDequip
 		jmp end_switch
+	shop:
+		mShop
+		jmp end_switch
 	end_switch:
 	jmp while_main ;//default
 
 
-	; endwhile_main:
+	 endwhile_main:
 	
 	invoke ExitProcess, 0
 
